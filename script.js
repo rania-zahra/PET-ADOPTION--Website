ef348cce776d33973b701bf02fc7e4ce6c617869
Rania <11raniazahra@gmail.comgit config --listgit config user.name>
2026-09-23 10:45:31 -0700

    addition ki hai

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 alert("hello");
 let pets=[
-     {name:"Tommy", breed:"labrador", age:"2 years"},
-{name:"luna", breed:"Perisan cat",  age:"1 years"},
-{name:"Jenny", breed:"German Shepherd", age:"2 years"}
+     {name:"Tommy", 
+    breed:"labrador",
+     age:"2 years", 
+     description:"friendly and active dog",
+image:"tommy.jpg",
+gender:"male",
+health:"healthy"
+     },
+{name:"luna", 
+breed:"Perisan cat", 
+ age:"1 years",
+ description:"friendly and naugthy",
+image:"luna.jpg",
+gender:"female",
+health:"healthy"
+},
+{name:"Jenny",
+ breed:"German Shepherd",
+ age:"2 years",
+ description:"decent and cute",
+image:"jenny.jpg",
+gender:"female",
+health:"healthy"
+
+}
 ];
 window.onload = function(){
 showPets(pets);
@@ -13,7 +35,10 @@ function showPets(petList){
         html += `<div class="pet-card">
         <h4> ${pet.name}</h4>
         <p>Breed: ${pet.breed}</p>
-        <p>Age: ${pet.age}</p>
+ <p>description: ${pet.description}</p>
+<h3>Image: ${pet.image}</h3>
+<p>Gender: ${pet.gender}</p>
+<p>Health: ${pet.health}</p>
         <button onclick="adoptPet('${pet.name}')">Adopt me </button></div>`;
     });
 document.getElementById("petList").innerHTML=html;
@@ -27,7 +52,7 @@ let filtered=pets.filter(pet =>
 showPets(filtered);
  }
 function adoptPet(petname){
-alert("you want to adopt :"    +   petname   +   "! Admin will ; contact you soon");}
+window.location.href = "adoption.html?pet=" +encodeURIComponent(petname);}
 function givesection(){
 document.getElementById("adopt-section").style.display ="none";
 document.getElementById("give-section").style.display ="block";
@@ -45,8 +70,11 @@ function submitPet() {
     let name = document.getElementById("petName").value;
     let breed = document.getElementById("petBreed").value;
     let age = document.getElementById("petAge").value;
-
-    if (name === "" || breed === "" || age === "") {
+let description = document.getElementById("petDescription").value;
+let image = document.getElementById("petImage").value;
+let gender = document.getElementById("petGender").value;
+let health = document.getElementById("petHealth").value;
+    if (name === "" || breed === "" ||  age === "" || description === "" || image === "" || gender === "" || health === "" ) {
         alert("Please fill all fields!");
         return;
     }
@@ -55,5 +83,9 @@ function submitPet() {
         "Pet submitted successfully!\n" +
         "Name: " + name + "\n" +
         "Breed: " + breed + "\n" +
-        "Age: " + age
+        "Age: " + age + "\n" +
+     "Description:" + description +"\n"+
+     "Image: " + image + "\n" +
+     "Gender: " + gender + "\n" +
+     "Health: " + health 
     );}
